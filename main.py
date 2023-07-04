304958196ae25fd03294267a37cda4bce7b4dba6
Aleksei Tishkov <tscenic.ua@gmail.com>
2023-07-04 16:11:20 +0800

    Изменен смысол бота. Теперь ищет числа.

--- main.py
+++ main.py
@@ -1,36 +1,37 @@
-import aiogram, requests
+import aiogram
+from aiogram.filters import BaseFilter, Text
+from aiogram.types import Message
+
 
 with open('API.txt', 'r', encoding='utf-8') as file:
     BOT_TOKEN: str = file.read()
 
-API_URL: str = 'https://api.telegram.org/bot'
-API_DOGS_URL: str = 'https://random.dog/woof.json'
-ERROR_TEXT: str = 'Тут должна была быть собащка, но пока не пришла...('
-
-TEXT: str = 'Было совершено НОСИЛИЕ'
-MAX_COUNTER = 100000
-cat_response: requests.Response
-cat_link: str
-
-offset: int = -2
-counter: int = 0
-chat_id: int
-timeout: int = 60
+bot: aiogram.Bot = aiogram.Bot(token=BOT_TOKEN)
+dp: aiogram.Dispatcher = aiogram.Dispatcher()
 
 
-while counter < MAX_COUNTER:
+class NumsInMsg(BaseFilter):
+    async def __call__(self, msg: Message) -> bool | dict[str, list[int]]:
+        nums = []
+        for word in msg.text.split():
+            w = word.replace('.', '').replace(',', '').strip()
+            if w.isdigit():
+                nums.append(int(w))
+        if nums:
+            return {'nums': nums}
+        return False
 
-    print('attempt =', counter)  #Чтобы видеть в консоли, что код живет
 
-    updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}&timeout={timeout}').json()
+@dp.message(Text(startswith='Ищи числа', ignore_case=True), NumsInMsg())
+async def process_in_nums(message: Message, nums: list[int]):
+    await message.answer(text=f'Нашел вот это: {", ".join(str(i) for i in nums)}')
 
-    if updates['result']:
-        for result in updates['result']:
-            offset = result['update_id']
-            chat_id = result['message']['from']['id']
-            dog_response = requests.get(API_DOGS_URL)
-            if dog_response.status_code == 200:
-                requests.get(f'{API_URL}{BOT_TOKEN}/sendPhoto?chat_id={chat_id}&photo={dog_response.json()["url"]}')
-            else:
-                requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={ERROR_TEXT}')
-    counter += 1
+
+@dp.message(Text(startswith='Ищи числа', ignore_case=True))
+async def process_in_nums(message: Message):
+    await message.answer(text='Нет тут ничего')
+
+
+if __name__ == '__main__':
+    dp.run_polling(bot)
+
